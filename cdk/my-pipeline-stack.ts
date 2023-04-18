6c53fe1f3e1ca7e20ff0c1d5828cf2466439bd57
Steve <8792575+CloudScubaSteve@users.noreply.github.com>
2023-04-18 16:16:29 -0400

    cdk app test

--- cdk/my-pipeline-stack.ts
+++ cdk/my-pipeline-stack.ts
@@ -9,45 +9,21 @@ import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelin
 import {SecretValue} from "aws-cdk-lib";
 import {CodeStarConnectionsSourceAction} from "aws-cdk-lib/aws-codepipeline-actions";
 
-// const githubConnection = new codestarconnections.CfnConnection(this, 'GitHubConnection', {
-//   connectionName: 'my-github-connection',
-//   providerType: 'GitHub',
-//   owner: 'my-github-username',
-//   connectionProperties: {
-//     'accessToken': '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'
-//   }
-// });
-//
-// const sourceOutput = new codepipeline.Artifact();
-//
-// const sourceAction = new codepipelineActions.CodeStarConnectionsSourceAction({
-//   actionName: 'Source',
-//   owner: 'CloudScubaSteve',
-//   repo: 'cloud_code',
-//   output: sourceOutput,
-//   connectionArn: githubConnection.attrConnectionArn
-// });
-
-export class MyPipelineStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-	// question - how do I create a codestar connection for version 2 of github connectors,
-	// and consume it when setting up the pipeline?
-	// new CodeStarConnectionsSourceAction()
-
-	  const pipeline = new CodePipeline(this, 'Pipeline', {
-		  pipelineName: 'MyPipeline',
-		  synth: new ShellStep('Synth', {
-			  input: codepipelineActions.CodeStarConnectionsSourceAction({
-                       actionName: 'Source',
-                       owner: 'CloudScubaSteve',
-                       repo: 'cloud_code',
-                       output: sourceOutput,
-                       connectionArn: githubConnection.attrConnectionArn
-			  }),
-			  commands: ['npm ci', 'npm run build', 'npx cdk synth']
-		  })
-	  });
+const githubConnection = new codestarconnections.CfnConnection(this, 'GitHubConnection', {
+  connectionName: 'my-github-connection',
+  providerType: 'GitHub',
+  owner: 'CloudScubaSteve',
+  connectionProperties: {
+    'accessToken': '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'
   }
-}
+});
+
+const sourceOutput = new codepipeline.Artifact();
+
+const sourceAction = new codepipelineActions.CodeStarConnectionsSourceAction({
+  actionName: 'Source',
+  owner: 'CloudScubaSteve',
+  repo: 'cloud_code',
+  output: sourceOutput,
+  connectionArn: githubConnection.attrConnectionArn
+});
